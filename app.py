b804cc8907bdb42655b43af6f2a1ff5947163982
Nikunj Zala <zalanikunj1204@gmail.com>
2026-03-22 13:25:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ if gemini_key:
         }
 
         llm = ChatGoogleGenerativeAI(
-            model="gemini-1.5-flash", # Using stable 1.5-flash
+            model="gemini-2.5-flash-lite", # Using stable 1.5-flash
             google_api_key=gemini_key,
             temperature=0,
             safety_settings=safety_cfg
